dcedb4119540fbf18516f12378b2e558ae7c0ca1
Laurian Gridinoc <laurian@gridinoc.name>
2013-10-16 14:56:26 +0200

    CORS

--- packages/api/app.js
+++ packages/api/app.js
@@ -33,6 +33,7 @@ var options = {
 var allowCrossDomain = function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header('Access-Control-Allow-Methods', 'OPTIONS, POST, GET, PUT, DELETE');
   next();
 }
 
@@ -53,8 +54,36 @@ app.use(express.cookieParser('xaifeeK0Xoo1Oghahfu8WeeShooqueeG'));
 app.use(express.session());
 app.use(passport.initialize());
 app.use(passport.session());
+
+//http://stackoverflow.com/questions/7067966/how-to-allow-cors-in-express-nodejs
+app.use(function(req, res, next) {
+    var oneof = false;
+    if(req.headers.origin) {
+        res.header('Access-Control-Allow-Origin', req.headers.origin);
+        oneof = true;
+    }
+    if(req.headers['access-control-request-method']) {
+        res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
+        oneof = true;
+    }
+    if(req.headers['access-control-request-headers']) {
+        res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
+        oneof = true;
+    }
+    if(oneof) {
+        res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365);
+    }
+
+    // intercept OPTIONS method
+    if (oneof && req.method == 'OPTIONS') {
+        res.send(200);
+    }
+    else {
+        next();
+    }
+});
   
-app.use(allowCrossDomain);  
+// app.use(allowCrossDomain);  
 app.use(app.router);
 
 app.use(require('less-middleware')({ src: __dirname + '/public' }));
