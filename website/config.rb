164c982bd9596c24b94de246b1326375d4fd56fc
Piotr <piotr@Retinavi.local>
2013-10-26 03:01:15 +0200

    Image optim on build.

--- website/config.rb
+++ website/config.rb
@@ -44,24 +44,24 @@ sprockets.append_path 'assets/vendor'
 # activate :automatic_image_sizes
 
 # Activate Image Optim
-# activate :image_optim do |image_optim|
-#   # print out skipped images
-#   image_optim.verbose = false
-#   # Setting these to true or nil will let image_optim determine them (recommended)
-#   image_optim.nice = true
-#   image_optim.threads = true
-#   # Image extensions to attempt to compress
-#   image_optim.image_extensions = %w(.png .jpg .gif)
-#   # compressor worker options, individual optimisers can be disabled by passing
-#   # false instead of a hash
-#   image_optim.pngcrush_options  = {:chunks => ['alla'], :fix => false, :brute => false}
-#   image_optim.pngout_options    = {:copy_chunks => false, :strategy => 0}
-#   image_optim.optipng_options   = {:level => 6, :interlace => false}
-#   image_optim.advpng_options    = {:level => 4}
-#   image_optim.jpegoptim_options = {:strip => ['all'], :max_quality => 100}
-#   image_optim.jpegtran_options  = {:copy_chunks => false, :progressive => true, :jpegrescan => true}
-#   image_optim.gifsicle_options  = {:interlace => false}
-# end
+activate :image_optim do |image_optim|
+  # print out skipped images
+  image_optim.verbose = false
+  # Setting these to true or nil will let image_optim determine them (recommended)
+  image_optim.nice = true
+  image_optim.threads = true
+  # Image extensions to attempt to compress
+  image_optim.image_extensions = %w(.png .jpg .gif)
+  # compressor worker options, individual optimisers can be disabled by passing
+  # false instead of a hash
+  image_optim.pngcrush_options  = {:chunks => ['alla'], :fix => false, :brute => false}
+  image_optim.pngout_options    = {:copy_chunks => false, :strategy => 0}
+  image_optim.optipng_options   = {:level => 6, :interlace => false}
+  image_optim.advpng_options    = {:level => 4}
+  image_optim.jpegoptim_options = {:strip => ['all'], :max_quality => 100}
+  image_optim.jpegtran_options  = {:copy_chunks => false, :progressive => true, :jpegrescan => true}
+  image_optim.gifsicle_options  = {:interlace => false}
+end
 
 # Reload the browser automatically whenever files change
 activate :livereload
