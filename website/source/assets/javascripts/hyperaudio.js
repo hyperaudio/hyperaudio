219ceaaea129619681070a3a8dd49f943b725fbe
Piotr <piotr@Retinavi.local>
2013-10-26 15:20:40 +0200

    Obfuscate footer contact email.

--- website/source/assets/javascripts/hyperaudio.js
+++ website/source/assets/javascripts/hyperaudio.js
@@ -1,15 +1,39 @@
-$( '.slider' ).cycle({
-  autoHeight: 'calc',
-  slides: '.slides > div',
-  log: false,
-  next: '.slider-next',
-  prev: '.slider-prev',
-  pager: '.slider-pages',
-  pagerActiveClass: 'active',
-  pagerTemplate: '<li class="slider-page"></li>',
-  pauseOnHover: true,
-  slideActiveClass: 'active',
-  slideClass: 'slide',
-  speed: 1000,
-  timeout: 5000
+
+$( document ).ready(function() {
+
+  $('.slider').cycle({
+    autoHeight: 'calc',
+    slides: '.slides > div',
+    log: false,
+    next: '.slider-next',
+    prev: '.slider-prev',
+    pager: '.slider-pages',
+    pagerActiveClass: 'active',
+    pagerTemplate: '<li class="slider-page"></li>',
+    pauseOnHover: true,
+    slideActiveClass: 'active',
+    slideClass: 'slide',
+    speed: 1000,
+    timeout: 5000
+  });
+
+  // Email obfuscation
+
+  var emailize;
+  $.fn.email = function() {
+    return this.each(function() {
+      var e, i;
+      i = $(this).attr('href');
+      e = emailize($(this).attr('href'));
+      return $(this).attr('href', "mailto:" + e);
+    });
+  };
+  emailize = function(name) {
+    var dom;
+    dom = 'hyperaud';
+    return "" + (name.toLowerCase()) + "@" + dom + ".io";
+  };
+
+  $('a.email').email()
+
 });
