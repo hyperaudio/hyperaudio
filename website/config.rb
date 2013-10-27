0cb8273ba967216f8d0e18e49df0c8a42afc6399
Piotr <piotr@Retinavi.local>
2013-10-27 20:21:37 +0100

    Minimize javascript no more.

--- website/config.rb
+++ website/config.rb
@@ -69,7 +69,7 @@ activate :livereload
 # Build-specific configuration
 configure :build do
   activate :minify_css
-  activate :minify_javascript
+  # activate :minify_javascript
   activate :relative_assets
 end
 
