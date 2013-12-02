827fad8e5d46da2c559d870adce028714ce46e7b
Laurian Gridinoc <laurian@gridinoc.name>
2013-12-02 05:45:51 +0000

    init: “yo backbone HA”

---
+++ packages/dashboard/app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global haDash, $*/
+
+
+window.haDash = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    haDash.init();
+});
