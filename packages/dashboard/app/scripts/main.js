bff26c6ecb1beebc18ba5fc316fd67009c3cf7a8
Laurian Gridinoc <laurian@gridinoc.name>
2013-12-05 15:49:23 +0000

    more Foundation

--- packages/dashboard/app/scripts/main.js
+++ packages/dashboard/app/scripts/main.js
@@ -51,18 +51,20 @@ $(document).ready(function() {
 	'use strict';
 
 	haDash.init();
+	
+	$(document).foundation();
 });
 
-// function fixTooltips() {
-// 	$(document).foundation({
-// 	  tooltips: {
-// 	    selector : '.has-tip',
-// 	    additional_inheritable_classes : [],
-// 	    tooltip_class : '.tooltip',
-// 	    touch_close_text: 'tap to close',
-// 	    disable_for_touch: false,
-// 	    tip_template : function (selector, content) {
-// 	      return '<span data-selector="' + selector + '" class="' + Foundation.libs.tooltips.settings.tooltipClass.substring(1) + '">' + content + '<span class="nub"></span></span>';
-// 	  }}
-// 	});
-// }
+function fixTooltips() {
+	$(document).foundation({
+	  tooltips: {
+	    selector : '.has-tip',
+	    additional_inheritable_classes : [],
+	    tooltip_class : '.tooltip',
+	    touch_close_text: 'tap to close',
+	    disable_for_touch: false,
+	    tip_template : function (selector, content) {
+	      return '<span data-selector="' + selector + '" class="' + Foundation.libs.tooltips.settings.tooltipClass.substring(1) + '">' + content + '<span class="nub"></span></span>';
+	  }}
+	});
+}
