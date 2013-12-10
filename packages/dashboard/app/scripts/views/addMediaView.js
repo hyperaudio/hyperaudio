7a035fea1645847f72c9aa600653fa0dce691aa3
Laurian Gridinoc <laurian@gridinoc.name>
2013-12-10 10:28:47 +0000

    add YT with title and desc and save back

--- packages/dashboard/app/scripts/views/addMediaView.js
+++ packages/dashboard/app/scripts/views/addMediaView.js
@@ -21,8 +21,54 @@ haDash.Views = haDash.Views || {};
 			this.$el.foundation('reveal', 'open');
 			
 			return this;
+		},
+		
+		events: {
+			"click button": "addYT"
+		},
+		
+		addYT: function() {
+			var ytUrl = new URI(this.$el.find('input').val());			
+			var ytID = ytUrl.search(true)['v'];
+			
+			console.log(ytID);
+			
+			var model = this.model;
+			
+			$.ajax({
+				url: "http://gdata.youtube.com/feeds/api/videos/" + ytID + "?v=2&alt=json",
+				success: function(ytData) {
+					console.log(ytData);
+					
+					model.set('owner', haDash.user.username);
+					model.set('label', ytData.entry.title["$t"]);
+					model.set('desc', ytData.entry["media$group"]["media$description"]["$t"]);
+					model.set('source', {
+						"youtube": {
+						      "type": "video/youtube",
+						      "url": "http://www.youtube.com/watch?v=" + ytID
+						 }
+					});
+					
+					console.log(model);
+					
+					haDash.mediaListView.collection.push(model);
+					
+					model.save();
+				}
+			});
 		}
 
     });
 
 })();
+
+/*
+					label: "Empty",
+					desc: "no content",
+					type: "text",
+					// sort: 0,
+					owner: null,
+					meta: {},
+					transcripts: []
+*/
