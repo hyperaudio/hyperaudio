478ac4c7e7d347f22674a73a239591d47c7da388
Laurian Gridinoc <laurian@gridinoc.name>
2013-12-11 15:45:49 +0000

    port 80 for now

--- packages/api/app.js
+++ packages/api/app.js
@@ -41,7 +41,7 @@ var app = express();
 
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 80);
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
