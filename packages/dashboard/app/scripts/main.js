3b11c418f471225d36621c6fe05f6da7b4b2bb05
Laurian Gridinoc <laurian@gridinoc.name>
2014-01-01 23:27:28 +0000

    no more ws

--- packages/dashboard/app/scripts/main.js
+++ packages/dashboard/app/scripts/main.js
@@ -80,15 +80,15 @@ window.haDash = {
   socket: null,
 
   socketConnect: function () {
-    haDash.socket = io.connect('//api.hyperaud.io');
+    // haDash.socket = io.connect('//api.hyperaud.io/');
 
-    haDash.socket.on(haDash.user, function (data) {
-      console.log(data);
-    });
+    // haDash.socket.on(haDash.user, function (data) {
+    //   console.log(data);
+    // });
   },
 
   socketDisconnect: function () {
-    if (haDash.socket) haDash.socket.disconnect();
+    // if (haDash.socket) haDash.socket.disconnect();
   }
 };
 
