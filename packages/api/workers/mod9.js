75bac586035e7a61649d1e8aac914d8cbd960d7e
Laurian Gridinoc <laurian@gridinoc.name>
2014-01-01 19:02:52 +0000

    no ws

--- packages/api/workers/mod9.js
+++ packages/api/workers/mod9.js
@@ -7,7 +7,7 @@ var Transcript = require('../models/transcript');
 
 mongoose.connect("mongodb://localhost/hyperaudio01"); //FIXME conf
 
-var socket = require('socket.io-client')('http://api.hyperaud.io');
+// var socket = require('socket.io-client')('http://api.hyperaud.io');
 
 module.exports = function() {
   function ProbeHandler() {
@@ -48,7 +48,7 @@ module.exports = function() {
           try {
             // data = part + data;
             result.push([process.hrtime(), JSON.parse(data)]);
-            socket.emit(payload.owner, JSON.parse(data)]);
+            // socket.emit(payload.owner, JSON.parse(data)]);
             // part = "";
           } catch (err) {
             console.log('err skipping');
