3399fb7b7afc7973b2a0c9388768be72d750b2fa
Laurian Gridinoc <laurian@gridinoc.name>
2014-01-22 13:32:41 +0300

    no tags on null tags

--- packages/api/media.js
+++ packages/api/media.js
@@ -77,14 +77,14 @@ module.exports = function(app, nconf) {
     if (req.params.user) {
       var query = {
         owner: req.params.user,
-        tags: []
+        $or: [{tags: []}, {tags: null}]
       };
       return MediaObject.find(query, function(err, mediaObjects) {
         return res.send(mediaObjects);
       });
     }
     var query = {
-      tags: []
+      $or: [{tags: []}, {tags: null}]
     };
     return MediaObject.find(query,function(err, mediaObjects) {
       return res.send(mediaObjects);
