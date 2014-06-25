e58b68a0d67c27380baa75c7042c958140344e55
Laurian Gridinoc <laurian@gridinoc.name>
2014-06-25 14:31:34 -0700

    random code, maybe it works

--- packages/api/media.js
+++ packages/api/media.js
@@ -213,8 +213,9 @@ module.exports = function(app, nconf) {
     return Transcript.find({
       media: mediaObject
     }, function(err, _transcripts) {
+        console.log(mediaObject);
         console.log(err);
-        console.log(_transcripts);
+        // console.log(_transcripts);
        // return transcriptsOf(mediaObjects, transcripts.concat(_transcripts));
        // return transcripts.concat(JSON.parse(JSON.stringify(_transcripts)));
        // return _transcripts;
