dfbf22afed4afa8b44d47032df6899fbef9c6638
Laurian Gridinoc <laurian@gridinoc.name>
2014-07-10 12:49:28 +0100

    Revert "cookie domain test"
    This reverts commit 815f1f365cf22f368a8b5d170e2b217bb5909653.

--- packages/api/app.js
+++ packages/api/app.js
@@ -47,12 +47,11 @@ app.use(express.methodOverride());
 var sessions = require("client-sessions");
 app.use(sessions({
   cookieName: 'session',
-  secret: 'exzohziuchaepah7xie0vei6Apai8aep4th', //FIXME: move to conf
+  secret: 'ohziuchaepah7xie0vei6Apai8aep4th', //FIXME: move to conf
   duration: 24 * 60 * 60 * 1000, // how long the session will stay valid in ms
   activeDuration: 1000 * 60 * 5, // if expiresIn < activeDuration, the session will be extended by activeDuration milliseconds
   cookie: {
     path: '/v1', // cookie will only be sent to requests under '/v1'
-  //  domain: 'api.hyperaud.io',
     // maxAge: 60000, // duration of the cookie in milliseconds, defaults to duration above
     ephemeral: false, // when true, cookie expires when the browser closes
     httpOnly: true, // when true, cookie is not accessible from javascript
