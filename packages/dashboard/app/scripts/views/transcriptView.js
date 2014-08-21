538246d9c95468e12c4852ab5a296d033b9b0837
Laurian Gridinoc <laurian@gridinoc.name>
2014-08-21 19:55:13 +0300

    salted transcript url

--- packages/dashboard/app/scripts/views/transcriptView.js
+++ packages/dashboard/app/scripts/views/transcriptView.js
@@ -88,7 +88,7 @@ haDash.Views = haDash.Views || {};
         var self = this;
         this.refreshing = setInterval(function() {
           self.model.fetch({
-            url: haDash.API + '/transcripts/' + self.model.id,
+            url: haDash.API + '/transcripts/' + self.model.id + '?salt=' + Math.random(),
             success: function(model) {
               console.log(model);
               // self.render();
