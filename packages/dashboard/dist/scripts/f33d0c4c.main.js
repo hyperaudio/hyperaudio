23ee32e946891e2dda13285c7c93d5fa24026e0c
Laurian Gridinoc <laurian@gridinoc.name>
2014-09-20 10:55:17 +0300

    Revert "test commit"
    This reverts commit a05c7f0baddf815e1645e8a4593511c2940f76a0.

--- packages/dashboard/dist/scripts/f33d0c4c.main.js
+++ packages/dashboard/dist/scripts/f33d0c4c.main.js
@@ -263,7 +263,7 @@ __p += '\n\n<p>&nbsp;</p>\n\n<div id="transcripts"></div>\n\n';
 return __p
 };
 
-this["JST"]["app/scripts/templates/medialist.ejs"] = function(obj) {
+this["JST"]["app/scripts/templates/mediaList.ejs"] = function(obj) {
 obj || (obj = {});
 var __t, __p = '', __e = _.escape;
 with (obj) {
@@ -347,7 +347,7 @@ __p += '<div class="user your">\n\n  <a class="button primary" href="/pad/">Crea
 return __p
 };
 
-this["JST"]["app/scripts/templates/signin.ejs"] = function(obj) {
+this["JST"]["app/scripts/templates/signIn.ejs"] = function(obj) {
 obj || (obj = {});
 var __t, __p = '', __e = _.escape;
 with (obj) {
@@ -357,7 +357,7 @@ __p += '<hgroup class="section-head">\n  <h1 class="section-head-heading">\n    
 return __p
 };
 
-this["JST"]["app/scripts/templates/signup.ejs"] = function(obj) {
+this["JST"]["app/scripts/templates/signUp.ejs"] = function(obj) {
 obj || (obj = {});
 var __t, __p = '', __e = _.escape;
 with (obj) {
