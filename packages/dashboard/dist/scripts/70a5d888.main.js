fc334cd58fb05e76847d967ef559af2fc97e9099
maboa <mark.boas@gmail.com>
2014-11-18 17:11:38 +0100

    test changing router to check capitilisation#

--- packages/dashboard/dist/scripts/70a5d888.main.js
+++ packages/dashboard/dist/scripts/70a5d888.main.js
@@ -374,7 +374,7 @@ __p += '<hgroup class="section-head">\n  <h1 class="section-head-heading">\n    
 return __p
 };
 
-this["JST"]["app/scripts/templates/signin.ejs"] = function(obj) {
+this["JST"]["app/scripts/templates/signIn.ejs"] = function(obj) {
 obj || (obj = {});
 var __t, __p = '', __e = _.escape;
 with (obj) {
@@ -384,7 +384,7 @@ __p += '<hgroup class="section-head">\n  <h1 class="section-head-heading">\n    
 return __p
 };
 
-this["JST"]["app/scripts/templates/signup.ejs"] = function(obj) {
+this["JST"]["app/scripts/templates/signUp.ejs"] = function(obj) {
 obj || (obj = {});
 var __t, __p = '', __e = _.escape;
 with (obj) {
@@ -548,7 +548,7 @@ haDash.Routers = haDash.Routers || {};
       'media/': 'media',
       'media/:id': 'mediaDetail',
 
-      'signin/': 'signin',
+      'signin/': 'signIn',
       'login/': 'signin',
 
       'signout/': 'signout',
