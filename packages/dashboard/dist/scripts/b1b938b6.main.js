7bae7d8640503dc164bf657ce8176a0650edc9c4
maboa <mark.boas@gmail.com>
2014-12-02 16:09:44 +0100

    testing

--- packages/dashboard/dist/scripts/b1b938b6.main.js
+++ packages/dashboard/dist/scripts/b1b938b6.main.js
@@ -276,11 +276,14 @@ if (source.youtube && source.youtube.url) {;
 __p += '\n<iframe width="420" height="315" src="http://www.youtube.com/embed/';
 print(_.escape(source.youtube.url.split('=')[1])) ;
 __p += '">\n</iframe>\n';
-} else if (source.mp4 && source.mp4.url) {
+} else if (source.mp4 && source.mp4.url) {;
+__p += '\n<video src="';
+print(_.escape(source.mp4.url)) ;
+__p += '">\n</video>\n';
 
 }
 ;
-__p += '\n\n\n</p>\n\n<p class="lead desc editable" data-field="desc">';
+__p += '\n\n\n\n\n</p>\n\n<p class="lead desc editable" data-field="desc">';
 
   var lines = desc.split("\n");
   for (var i = 0; i < lines.length; i++) {
