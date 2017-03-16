0a5d8315c41608055bde6f380d6b567085acc0c5
Laurian Gridinoc <laurian@gridinoc.name>
2017-03-16 15:05:39 +0000

    accept 50M as per https://stackoverflow.com/questions/19917401/error-request-entity-too-large

--- packages/api/app.js
+++ packages/api/app.js
@@ -32,8 +32,8 @@ app.use(express.favicon());
 app.use(express.logger('dev'));
 
 // app.use(express.bodyParser());
-app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
 
 app.use(express.methodOverride());
 
