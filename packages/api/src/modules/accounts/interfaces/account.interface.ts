564b4a3db70f0ba9a6ddd447754dc2b6f2217f7c
Laurian Gridinoc <laurian@gridinoc.name>
2017-11-06 18:19:32 +0100

    super _id

--- packages/api/src/modules/accounts/interfaces/account.interface.ts
+++ packages/api/src/modules/accounts/interfaces/account.interface.ts
@@ -1,7 +1,7 @@
 import { Schema, Document } from 'mongoose';
 
 export interface Account extends Document {
-  readonly _id: String;
+  // readonly _id: String;
   readonly meta: Schema.Types.Mixed;
   readonly email: String;
   readonly token: String;
