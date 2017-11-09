822326982fd2f7d5ad806e66ad36ccc633cb782c
Laurian Gridinoc <laurian@gridinoc.name>
2017-11-09 18:54:39 +0100

    auth x-user

--- packages/api/src/modules/middleware/auth.middleware.ts
+++ packages/api/src/modules/middleware/auth.middleware.ts
@@ -5,12 +5,12 @@ import * as jwt from 'jsonwebtoken';
 export class AuthMiddleware implements NestMiddleware {
   resolve(): ExpressMiddleware {
     return (req, res, next) => {
-      // if (req.req.getHeader('Authorization')) {
-      //   try {
-      //     const decoded = jwt.verify(req.getHeader('Authorization'), process.env.JWT_SECRET);
-      //     res.set('X-User', decoded.user);
-      //   } catch (ignored) {}
-      // }
+      if (req.get('Authorization')) {
+        try {
+          const decoded = jwt.verify(req.get('Authorization'), process.env.JWT_SECRET);
+          res.set('X-User', decoded.user);
+        } catch (ignored) {}
+      }
 
       next();
     };
