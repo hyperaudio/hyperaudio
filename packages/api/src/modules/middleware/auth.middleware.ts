ba8e9f65401511d846ff1cdff80db02b3c1ca841
Laurian Gridinoc <laurian@gridinoc.name>
2017-11-10 16:20:54 +0100

    X-User

--- packages/api/src/modules/middleware/auth.middleware.ts
+++ packages/api/src/modules/middleware/auth.middleware.ts
@@ -7,7 +7,7 @@ export class AuthMiddleware implements NestMiddleware {
     return (req, res, next) => {
       if (req.get('Authorization')) {
         try {
-          const decoded = jwt.verify(req.get('Authorization'), process.env.JWT_SECRET);
+          const decoded = jwt.verify(req.get('Authorization').split(' ').pop(), process.env.JWT_SECRET);
           res.set('X-User', decoded.user);
         } catch (ignored) {}
       }
