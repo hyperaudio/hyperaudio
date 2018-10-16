6b35a5b68a7bc14a5a59d71eec20c120ac91d210
Laurian Gridinoc <laurian@gridinoc.name>
2018-10-16 21:44:33 +0200

    "Player"

---
+++ packages/player-concept/src/Player.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Player.css';
+
+class Player extends Component {
+  render() {
+    return (
+      <div className="Player">
+
+      </div>
+    );
+  }
+}
+
+export default Player;
--- packages/player-concept/src/Player.test.js
+++ packages/player-concept/src/Player.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Player from './Player';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Player />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
