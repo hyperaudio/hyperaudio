d9555c71956f70952243009565c25208bedc5c9d
Piotr F <piotr@piotrf.pl>
2018-10-17 18:37:26 +0200

    Add reset mixin.

--- packages/ui/mixins/index.js
+++ packages/ui/mixins/index.js
@@ -1,5 +1,6 @@
 export * from "./disselect";
 export * from "./paint";
+export * from "./reset";
 export * from "./setSize";
 export * from "./setSpace";
 export * from "./setType";
---
+++ packages/ui/mixins/reset.js
@@ -0,0 +1,15 @@
+export const reset = `
+  border: 0;
+  box-sizing: border-box;
+  font-size: 100%;
+  font: inherit;
+  line-height: 1;
+  margin: 0;
+  padding: 0;
+  vertical-align: baseline;
+  * {
+    box-sizing: border-box;
+  }
+`;
+
+export default reset;
