078e537b7b0635fb37fdc10b13942e94d479b7e1
Piotr F <hello@piotrf.pl>
2020-11-20 16:38:42 +0100

    Allow importing svgs.

--- website/next.config.js
+++ website/next.config.js
@@ -6,6 +6,10 @@ module.exports = withMDX({
   pageExtensions: ['js', 'jsx', 'mdx'],
   webpack: (config, { isServer }) => {
     // Fixes npm packages (mdx) that depend on `fs` module
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
     if (!isServer) {
       // eslint-disable-next-line no-param-reassign
       config.node = {
