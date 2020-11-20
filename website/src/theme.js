8bfd0c032061030141e4086a3e1a33ebe09135d4
Piotr F <hello@piotrf.pl>
2020-11-20 13:56:37 +0100

    Reinit theme.

--- website/src/theme.js
+++ website/src/theme.js
@@ -2,20 +2,20 @@ import { createMuiTheme } from '@material-ui/core/styles';
 import { red } from '@material-ui/core/colors';
 
 const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: '#512da8',
-    },
-    secondary: {
-      main: '#ff4081',
-    },
-    error: {
-      main: red.A400,
-    },
-    background: {
-      default: '#fff',
-    },
-  },
+  // palette: {
+  //   primary: {
+  //     main: '#512da8',
+  //   },
+  //   secondary: {
+  //     main: '#ff4081',
+  //   },
+  //   error: {
+  //     main: red.A400,
+  //   },
+  //   background: {
+  //     default: '#fff',
+  //   },
+  // },
 });
 
 export default theme;
