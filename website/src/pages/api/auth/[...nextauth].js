11ac443288384975fbb28e50e068a58b5d237833
Laurian Gridinoc <laurian@gridinoc.name>
2020-11-20 20:12:53 +0200

    Apple, FB

--- website/src/pages/api/auth/[...nextauth].js
+++ website/src/pages/api/auth/[...nextauth].js
@@ -9,10 +9,23 @@ const options = {
       from: process.env.EMAIL_FROM,
       sendVerificationRequest,
     }),
+    Providers.Apple({
+      clientId: process.env.APPLE_ID,
+      clientSecret: {
+        appleId: process.env.APPLE_ID,
+        teamId: process.env.APPLE_TEAM_ID,
+        privateKey: process.env.APPLE_PRIVATE_KEY,
+        keyId: process.env.APPLE_KEY_ID,
+      },
+    }),
     // Providers.Twitter({
     //   clientId: process.env.TWITTER_CLIENT_ID,
     //   clientSecret: process.env.TWITTER_CLIENT_SECRET,
     // }),
+    Providers.Facebook({
+      clientId: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+    }),
   ],
 
   jwt: { secret: process.env.JWT_SECRET },
