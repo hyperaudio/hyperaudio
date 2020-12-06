07b5f95234965d9b17c8b2c53412859d0c6c563d
Laurian Gridinoc <laurian@gridinoc.name>
2020-12-06 12:25:04 +0200

    Pinpoint analytics

--- website/src/App.js
+++ website/src/App.js
@@ -3,6 +3,7 @@ import 'reflect-metadata'; // FIXME: still need this?
 
 import App from 'next/app';
 import Amplify from 'aws-amplify';
+import Analytics from '@aws-amplify/analytics';
 
 import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -12,6 +13,17 @@ import awsconfig from './aws-config';
 import theme from './theme';
 
 Amplify.configure({ ...awsexports, ...awsconfig });
+
+Analytics.autoTrack('session', {
+  enable: true,
+});
+
+Analytics.autoTrack('pageView', {
+  enable: true,
+  // OPTIONAL, by default is 'multiPageApp'
+  // you need to change it to 'SPA' if your app is a single-page app like React
+  type: 'SPA',
+});
 
 export default class Application extends App {
   render() {
