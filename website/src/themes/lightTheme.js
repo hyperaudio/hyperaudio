9c1d7244632924e82faad0b649d1562d46658760
Piotr F <hello@piotrf.pl>
2021-01-08 15:24:56 +0100

    Bring in brand colors.

--- website/src/themes/lightTheme.js
+++ website/src/themes/lightTheme.js
@@ -10,6 +10,24 @@ const lightTheme = createMuiTheme({
     background: {
       default: 'white',
     },
+    primary: {
+      light: '#B07FEF',
+      main: '#6000DE',
+      dark: '#4C00D4',
+      contrastText: '#ffffff',
+    },
+    // secondary: {
+    //   light: '#FBC6B8',
+    //   main: '#F78C71',
+    //   dark: '#F5795E',
+    //   contrastText: '#ffffff',
+    // },
+    secondary: {
+      light: '#96E4DE',
+      main: '#2DC8BD',
+      dark: '#21BBAE',
+      contrastText: '#ffffff',
+    },
   },
   overrides: {
     MuiInputBase: {
@@ -22,10 +40,10 @@ const lightTheme = createMuiTheme({
     MuiInput: {
       underline: {
         '&:after': {
-          pointerEvents: 'default',
+          pointerEvents: 'auto',
         },
         '&:before': {
-          pointerEvents: 'default',
+          pointerEvents: 'auto',
         },
         '&$disabled:before': {
           borderBottomStyle: 'solid',
