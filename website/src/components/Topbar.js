dd12d34d84bd98b2c5329de5bcab1dbb21aa6732
Piotr F <hello@piotrf.pl>
2021-01-15 12:57:29 +0100

    Use poper HaIcon w/ IconButton in the Topbar.

--- website/src/components/Topbar.js
+++ website/src/components/Topbar.js
@@ -9,7 +9,6 @@ import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
-import Link from '@material-ui/core/Link';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import SettingsIcon from '@material-ui/icons/Settings';
@@ -18,9 +17,12 @@ import Tooltip from '@material-ui/core/Tooltip';
 import Typography from '@material-ui/core/Typography';
 import makeStyles from '@material-ui/core/styles/makeStyles';
 
-import HyperaudioIcon from 'src/assets/hyperaudio-icon.svg';
+import HyperaudioIcon from 'src/assets/icons/HaIcon';
 
 const useStyles = makeStyles(theme => ({
+  root: {
+    borderBottom: `1px solid ${theme.palette.divider}`,
+  },
   grow: {
     flexGrow: 1,
   },
@@ -68,28 +70,28 @@ const Topbar = () => {
 
   return (
     <>
-      <AppBar>
+      <AppBar color="transparent" className={classes.root} elevation={0}>
         <Toolbar>
           <Typography variant="h4" component="h1">
             <NextLink href="/" passHref>
-              <Link color="inherit">
+              <IconButton color="primary" edge="start">
                 <HyperaudioIcon />
-              </Link>
+              </IconButton>
             </NextLink>
           </Typography>
           <div className={classes.grow} />
           <NextLink href="/" passHref>
-            <Button color="inherit" variant="text">
+            <Button color="primary" variant="text">
               Home
             </Button>
           </NextLink>
           <NextLink href="/media" passHref>
-            <Button color="inherit" variant="text">
+            <Button color="primary" variant="text">
               Media
             </Button>
           </NextLink>
           <NextLink href="/mixes" passHref>
-            <Button color="inherit" variant="text">
+            <Button color="primary" variant="text">
               Mixes
             </Button>
           </NextLink>
@@ -100,14 +102,13 @@ const Topbar = () => {
                 <IconButton
                   aria-controls="simple-menu"
                   aria-haspopup="true"
-                  color="inherit"
+                  color="primary"
                   edge="end"
                   onClick={e => setAnchorEl(e.currentTarget)}
                 >
                   <SettingsIcon />
                 </IconButton>
               </Tooltip>
-
               <Menu
                 anchorEl={anchorEl}
                 anchorOrigin={{
