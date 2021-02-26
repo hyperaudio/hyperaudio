a17462601e9968d1525b65324ed165c7b7b9e4c3
Piotr F <hello@piotrf.pl>
2021-02-26 11:04:51 +0100

    Change hero text.

--- website/src/components/Hero.js
+++ website/src/components/Hero.js
@@ -1,6 +1,7 @@
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import makeStyles from '@material-ui/core/styles/makeStyles';
+
 import { Button } from '@material-ui/core';
 
 const useStyles = size =>
@@ -10,7 +11,9 @@ const useStyles = size =>
       color: theme.palette.primary.contrastText,
       paddingBottom: theme.spacing(size === 'small' ? 0.5 : 10),
       paddingTop: theme.spacing(size === 'small' ? 0.5 : 10),
-      textAlign: 'center',
+      paddingLeft: theme.spacing(4),
+      paddingRight: theme.spacing(4),
+      // textAlign: 'center',
     },
     title: {
       display: size === 'small' ? 'none' : 'block',
@@ -20,6 +23,7 @@ const useStyles = size =>
       marginBottom: theme.spacing(2),
       [theme.breakpoints.up('md')]: {
         marginBottom: theme.spacing(4),
+        maxWidth: '66%',
       },
     },
     button: {
@@ -29,18 +33,21 @@ const useStyles = size =>
 
 export default function Hero({ org, size }) {
   const classes = useStyles(size)();
+
   return (
     <div className={classes.root}>
       <Container>
-        <Typography className={classes.title} gutterBottom component="h1" variant="h3">
-          {org.meta.name}
-        </Typography>
-        <Typography className={classes.text} gutterBottom component="p" variant="h6">
-          {org.meta.text}
-        </Typography>
-        <Button className={classes.button} color="secondary" href={org.meta.url} variant="contained">
-          Visit {org.meta.name}
-        </Button>
+        <Container>
+          <Typography className={classes.title} gutterBottom component="h1" variant="h4">
+            {org.meta.name}
+          </Typography>
+          <Typography className={classes.text} gutterBottom component="p" variant="h6">
+            {org.meta.text}
+          </Typography>
+          <Button className={classes.button} href={org.meta.url} color="inherit" variant="outlined">
+            Visit {org.meta.name}
+          </Button>
+        </Container>
       </Container>
     </div>
   );
