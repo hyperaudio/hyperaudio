30c311b5439543534c25ce3317ecfacfda96387c
Laurian Gridinoc <laurian@gridinoc.name>
2021-09-05 16:02:54 +0100

    amplify fn

---
+++ website/amplify/backend/function/transcribe/src/index.js
@@ -0,0 +1,22 @@
+/* Amplify Params - DO NOT EDIT
+	ENV
+	REGION
+	STORAGE_HYPERAUDIO12034B8A_BUCKETNAME
+	STORAGE_HYPERAUDIODATA_ARN
+	STORAGE_HYPERAUDIODATA_NAME
+	STORAGE_HYPERAUDIODATA_STREAMARN
+Amplify Params - DO NOT EDIT */
+
+exports.handler = async event => {
+  // TODO implement
+  const response = {
+    statusCode: 200,
+    //  Uncomment below to enable CORS requests
+    //  headers: {
+    //      "Access-Control-Allow-Origin": "*",
+    //      "Access-Control-Allow-Headers": "*"
+    //  },
+    body: JSON.stringify('Hello from Lambda!'),
+  };
+  return response;
+};
