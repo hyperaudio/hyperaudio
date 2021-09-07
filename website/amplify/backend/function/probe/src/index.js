b7fbe31dfddbff92103720fc5ee895c56f7d05cb
Laurian Gridinoc <laurian@gridinoc.name>
2021-09-07 20:07:38 +0100

    probe fixes, ytdl py fn

--- website/amplify/backend/function/probe/src/index.js
+++ website/amplify/backend/function/probe/src/index.js
@@ -1,12 +1,41 @@
+const ytdl = require('ytdl-core');
+const ffprobe = require('ffprobe-client');
+const axios = require('axios');
+
 exports.handler = async event => {
-  // TODO implement
+  const {
+    queryStringParameters: { url },
+  } = event;
+
+  const data = {};
+
+  if (url) {
+    // ytdl
+    try {
+      data.ytdl = await ytdl.getInfo(url);
+    } catch (ignored) {}
+
+    // ffprobe
+    if (!data.ytdl)
+      try {
+        data.ffprobe = await ffprobe(url);
+      } catch (ignored) {}
+
+    // http head
+    if (!data.ytdl && !data.ffprobe)
+      try {
+        const { headers, status, statusText } = await axios.head(url);
+        data.http = { status, statusText, headers };
+      } catch (ignored) {}
+  }
+
   const response = {
     statusCode: 200,
     headers: {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Headers': '*',
     },
-    body: JSON.stringify(event),
+    body: JSON.stringify({ data }),
   };
   return response;
 };
---
+++ website/amplify/backend/function/ytdl/src/index.py
@@ -0,0 +1,16 @@
+import json
+
+
+def handler(event, context):
+    print("received event:")
+    print(event)
+
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Headers": "*",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+        },
+        "body": json.dumps(event),
+    }
