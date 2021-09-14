f21eb7d70ddcc5de951f8bd235b07454fbf9578f
Piotr F <hello@piotrf.pl>
2021-09-14 14:00:13 +0200

    Cleanup.

--- packages/remixer/src/index.js
+++ packages/remixer/src/index.js
@@ -1,27 +1,19 @@
 import React from 'react';
-import { Button, Typography } from '@mui/material';
 import { styled } from '@mui/material/styles';
 
-const PREFIX = 'Remixer';
-
-const classes = {
-  root: `${PREFIX}-root`,
-  sourcePane: `${PREFIX}-sourcePane`,
-  remixPane: `${PREFIX}-remixPane`,
-};
 const Root = styled('div')(({ theme }) => ({}));
-const SourcePane = styled('div')(({ theme }) => ({
+const Source = styled('div')(({ theme }) => ({
   background: 'blue',
 }));
-const RemixPane = styled('div')(({ theme }) => ({
+const Remix = styled('div')(({ theme }) => ({
   background: 'red',
 }));
 
 export const Remixer = ({ source, remix }) => {
   return (
-    <Root className={classes.root}>
-      <SourcePane>Source: {source}</SourcePane>
-      <RemixPane>Remix: {remix}</RemixPane>
+    <Root>
+      <Source>Source: {source}</Source>
+      <Remix>Remix: {remix}</Remix>
     </Root>
   );
 };
