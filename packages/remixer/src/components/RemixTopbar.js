c847b5889c575d75a3da26d8fea9071a2b802033
Piotr F <hello@piotrf.pl>
2021-09-23 19:42:08 +0200

    Stash RecursiveMenuItem progress.

--- packages/remixer/src/components/RemixTopbar.js
+++ packages/remixer/src/components/RemixTopbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useRef, useState } from 'react';
 
 import DeleteSweepIcon from '@mui/icons-material/DeleteSweep';
 import Divider from '@mui/material/Divider';
@@ -8,14 +9,73 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import LockIcon from '@mui/icons-material/Lock';
 import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
+import MenuItem, { MenuItemProps } from '@mui/material/MenuItem';
+import MenuList from '@mui/material/MenuList';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import Paper from '@mui/material/Paper';
+import Popper, { PopperProps } from '@mui/material/Popper';
 import RedoIcon from '@mui/icons-material/Redo';
 import Toolbar from '@mui/material/Toolbar';
 import Tooltip from '@mui/material/Tooltip';
 import UndoIcon from '@mui/icons-material/Undo';
+// import { createStyles, makeStyles } from '@mui/material';
+
+// const useStyles = makeStyles(theme =>
+//   createStyles({
+//     active: {
+//       backgroundColor: 'rgba(0, 0, 0, 0.04)',
+//     },
+//   }),
+// );
+
+type RecursiveMenuItemProps = MenuItemProps & {
+  button?: true,
+  label: string,
+} & Pick<PopperProps, 'placement'>;
+const RecursiveMenuItem = (props: RecursiveMenuItemProps) => {
+  // const classes = useStyles();
+  const [open, setOpen] = useState(false);
+  // const ref = (useRef < HTMLLIElement) | (null > null);
+  const ref = useRef();
+
+  return (
+    <MenuItem
+      {...props}
+      ref={ref}
+      MenuListProps={{
+        dense: true,
+      }}
+      transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+      anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+      // className={open ? classes.active : ''}
+      onMouseEnter={() => setOpen(true)}
+      // onFocus={() => setOpen(true)}
+      // onBlur={() => setOpen(false)}
+      onMouseLeave={() => setOpen(false)}
+    >
+      {props.label}
+      <Popper
+        anchorEl={ref.current}
+        open={open}
+        placement={props.placement ?? 'right'}
+        modifiers={{
+          flip: {
+            enabled: true,
+          },
+          preventOverflow: {
+            enabled: true,
+            boundariesElement: 'viewport',
+          },
+        }}
+      >
+        <Paper>{props.children}</Paper>
+      </Popper>
+    </MenuItem>
+  );
+};
 
 import { HideSourceIcon, ShareIcon, ShowSourceIcon } from '../icons';
+import { ClickAwayListener } from '@material-ui/core';
 
 export const RemixTopbar = props => {
   const { editable, showSource, setShowSource } = props;
@@ -60,8 +120,74 @@ export const RemixTopbar = props => {
                   <MoreHorizIcon fontSize="small" />
                 </IconButton>
               </Tooltip>
-
-              <Menu
+              {/* <ClickAwayListener onClickAway={onMoreClose}> */}
+              <Popper
+                anchorEl={anchorEl}
+                placement="bottom-end"
+                disablePortal={false}
+                open={open}
+                onClose={onMoreClose}
+                onClick={onMoreClose}
+                // transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                // anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+              >
+                <Paper
+                  elevation={0}
+                  sx={{
+                    overflow: 'visible',
+                    filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                    mt: 1.5,
+                    '&:before': {
+                      bgcolor: 'background.paper',
+                      content: '""',
+                      display: 'block',
+                      height: 10,
+                      position: 'absolute',
+                      right: 14,
+                      top: 0,
+                      transform: 'translateY(-50%) rotate(45deg)',
+                      width: 10,
+                      zIndex: 0,
+                    },
+                  }}
+                >
+                  <MenuList dense={true} /*autoFocus={true}*/>
+                    <RecursiveMenuItem
+                      autoFocus={false}
+                      label={
+                        <>
+                          <ListItemIcon>
+                            <DownloadIcon fontSize="small" color="primary" />
+                          </ListItemIcon>
+                          <ListItemText primary="Export" primaryTypographyProps={{ color: 'primary' }} />
+                        </>
+                      }
+                    >
+                      <MenuItem>
+                        <ListItemIcon>
+                          <LockIcon fontSize="small" color="primary" />
+                        </ListItemIcon>
+                        <ListItemText primary="Interactive Transcript" primaryTypographyProps={{ color: 'primary' }} />
+                      </MenuItem>
+                    </RecursiveMenuItem>
+                    <MenuItem>
+                      <ListItemIcon>
+                        <LockIcon fontSize="small" color="primary" />
+                      </ListItemIcon>
+                      <ListItemText primary="Make private" primaryTypographyProps={{ color: 'primary' }} />
+                    </MenuItem>
+                    <Divider />
+                    <MenuItem>
+                      <ListItemIcon>
+                        <DeleteSweepIcon fontSize="small" color="error" />
+                      </ListItemIcon>
+                      <ListItemText primary="Clear" primaryTypographyProps={{ color: 'error' }} />
+                    </MenuItem>
+                  </MenuList>
+                </Paper>
+              </Popper>
+              {/* </ClickAwayListener> */}
+              {/* <Menu
                 anchorEl={anchorEl}
                 open={open}
                 onClose={onMoreClose}
@@ -90,29 +216,7 @@ export const RemixTopbar = props => {
                   },
                 }}
                 variant="selectedMenu"
-                transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-                anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
-              >
-                <MenuItem>
-                  <ListItemIcon>
-                    <DownloadIcon fontSize="small" color="primary" />
-                  </ListItemIcon>
-                  <ListItemText primary="Export" primaryTypographyProps={{ color: 'primary' }} />
-                </MenuItem>
-                <MenuItem>
-                  <ListItemIcon>
-                    <LockIcon fontSize="small" color="primary" />
-                  </ListItemIcon>
-                  <ListItemText primary="Make private" primaryTypographyProps={{ color: 'primary' }} />
-                </MenuItem>
-                <Divider />
-                <MenuItem>
-                  <ListItemIcon>
-                    <DeleteSweepIcon fontSize="small" color="error" />
-                  </ListItemIcon>
-                  <ListItemText primary="Clear" primaryTypographyProps={{ color: 'error' }} />
-                </MenuItem>
-              </Menu>
+              ></Menu> */}
             </>
           )}
           <Tooltip title="Share remix">
