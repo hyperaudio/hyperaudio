86064a8b34727cda3869a407d6e8ab0b79ec00a9
Piotr F <hello@piotrf.pl>
2021-10-16 23:35:54 +0200

    Empty remix.

---
+++ packages/remixer/src/icons/StartDropIcon.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import SvgIcon from '@mui/material/SvgIcon';
+
+export const StartDropIcon = props => {
+  return (
+    <SvgIcon {...props}>
+      <polygon points="8.30578512 13.5867769 9.79338843 12.0991736 13.6115702 15.9173554 13.6115702 6.19834711 3 6.19834711 3 4.0661157 15.6942149 4.0661157 15.6942149 15.9173554 19.5123967 12.0991736 21 13.5867769 14.6528926 19.9338843"></polygon>
+    </SvgIcon>
+  );
+};
