6cece8ee2c6ce3c20d2e2f8af5c6bc2f3f8b60f6
Piotr F <hello@piotrf.pl>
2021-11-14 23:18:26 +0100

    Disable dragbar, add effect tooltips.

--- packages/remixer/src/components/Dragbar.js
+++ packages/remixer/src/components/Dragbar.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Droppable, Draggable as DraggableItem } from 'react-beautiful-dnd';
-import Draggable from 'react-draggable';
 
-import DragIndicatorIcon from '@mui/icons-material/DragIndicator';
+// import DragIndicatorIcon from '@mui/icons-material/DragIndicator';
 import Grid from '@mui/material/Grid';
 import IconButton from '@mui/material/IconButton';
 import MovieFilterIcon from '@mui/icons-material/MovieFilter';
 import SlideshowIcon from '@mui/icons-material/Slideshow';
 import TextFieldsIcon from '@mui/icons-material/TextFields';
+import Tooltip from '@mui/material/Tooltip';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import { styled } from '@mui/material/styles';
@@ -19,9 +19,9 @@ const classes = {
 };
 const Root = styled('div')(({ theme }) => ({
   background: theme.palette.background.paper,
-  borderRadius: theme.shape.borderRadius,
+  borderRadius: theme.shape.borderRadius * 2,
   bottom: 0,
-  boxShadow: theme.shadows[12],
+  boxShadow: theme.shadows[1],
   left: '50%',
   margin: theme.spacing(2),
   position: 'fixed',
@@ -61,86 +61,102 @@ const Effect = styled('div')(({ theme }) => ({
 // dragging a copy: https://github.com/atlassian/react-beautiful-dnd/issues/216#issuecomment-755864913
 export const Dragbar = props => {
   return (
-    <Draggable handle="#DragHandle" cancel={'[class*="MuiDialogContent-root"]'} bounds="#Layout">
-      <Root>
-        <Droppable droppableId={`droppable-toolbar`} type="BLOCK" isDropDisabled={true}>
-          {(provided, snapshot) => (
-            <div ref={provided.innerRef} {...provided.droppableProps}>
-              <Toolbar>
-                <Grid container columnSpacing={2} alignItems="center">
-                  <Grid item>
-                    <DragHandle size="small" color="default" id="DragHandle">
-                      <DragIndicatorIcon />
-                    </DragHandle>
+    <Root>
+      <Droppable droppableId={`droppable-toolbar`} type="BLOCK" isDropDisabled={true}>
+        {(provided, snapshot) => (
+          <div ref={provided.innerRef} {...provided.droppableProps}>
+            <Toolbar>
+              <Grid container columnSpacing={2} alignItems="center">
+                {/* <Grid item>
+                  <DragHandle size="small" color="default" id="DragHandle">
+                    <DragIndicatorIcon />
+                  </DragHandle>
+                </Grid> */}
+                <Grid item container xs columnSpacing={2}>
+                  <Grid item xs={4}>
+                    <DraggableItem draggableId="draggable-slides" index={0}>
+                      {(provided, snapshot) => (
+                        <>
+                          <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                            <Tooltip title="Drag this to add a slide to your remix">
+                              <Effect>
+                                <SlideshowIcon color="primary" fontSize="small" className={classes.effectIcon} />{' '}
+                                <Typography variant="subtitle2" color="primary">
+                                  Slide
+                                </Typography>
+                              </Effect>
+                            </Tooltip>
+                          </div>
+                          {snapshot.isDragging && (
+                            <Effect>
+                              <SlideshowIcon color="primary" fontSize="small" className={classes.effectIcon} />{' '}
+                              <Typography variant="subtitle2" color="primary">
+                                Slide
+                              </Typography>
+                            </Effect>
+                          )}
+                        </>
+                      )}
+                    </DraggableItem>
                   </Grid>
-                  <Grid item container xs columnSpacing={2}>
-                    <Grid item xs={4}>
-                      <DraggableItem draggableId="draggable-slides" index={0}>
-                        {(provided, snapshot) => (
-                          <>
-                            <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                  <Grid item xs={4}>
+                    <DraggableItem draggableId="draggable-title" index={1}>
+                      {(provided, snapshot) => (
+                        <>
+                          <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                            <Tooltip title="Drag this to add a title to your remix">
                               <Effect>
-                                <SlideshowIcon fontSize="small" color="inherit" className={classes.effectIcon} />{' '}
-                                <Typography variant="subtitle2">Slide</Typography>
+                                <TextFieldsIcon color="primary" fontSize="small" className={classes.effectIcon} />{' '}
+                                <Typography variant="subtitle2" color="primary">
+                                  Title
+                                </Typography>
                               </Effect>
-                            </div>
-                            {snapshot.isDragging && (
+                            </Tooltip>
+                          </div>
+                          {snapshot.isDragging && (
+                            <Effect>
+                              <TextFieldsIcon color="primary" fontSize="small" className={classes.effectIcon} />{' '}
+                              <Typography variant="subtitle2" color="primary">
+                                Title
+                              </Typography>
+                            </Effect>
+                          )}
+                        </>
+                      )}
+                    </DraggableItem>
+                  </Grid>
+                  <Grid item xs={4}>
+                    <DraggableItem draggableId="draggable-transition" index={2}>
+                      {(provided, snapshot) => (
+                        <>
+                          <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                            <Tooltip title="Drag this to add a transition to your remix">
                               <Effect>
-                                <SlideshowIcon fontSize="small" color="inherit" className={classes.effectIcon} />{' '}
-                                <Typography variant="subtitle2">Slide</Typography>
+                                <MovieFilterIcon color="primary" fontSize="small" className={classes.effectIcon} />{' '}
+                                <Typography variant="subtitle2" color="primary">
+                                  Transition
+                                </Typography>
                               </Effect>
-                            )}
-                          </>
-                        )}
-                      </DraggableItem>
-                    </Grid>
-                    <Grid item xs={4}>
-                      <DraggableItem draggableId="draggable-title" index={1}>
-                        {(provided, snapshot) => (
-                          <>
-                            <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-                              <Effect>
-                                <TextFieldsIcon fontSize="small" color="inherit" className={classes.effectIcon} />{' '}
-                                <Typography variant="subtitle2">Title</Typography>
-                              </Effect>
-                            </div>
-                            {snapshot.isDragging && (
-                              <Effect>
-                                <TextFieldsIcon fontSize="small" color="inherit" className={classes.effectIcon} />{' '}
-                                <Typography variant="subtitle2">Title</Typography>
-                              </Effect>
-                            )}
-                          </>
-                        )}
-                      </DraggableItem>
-                    </Grid>
-                    <Grid item xs={4}>
-                      <DraggableItem draggableId="draggable-transition" index={2}>
-                        {(provided, snapshot) => (
-                          <>
-                            <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-                              <Effect>
-                                <MovieFilterIcon fontSize="small" color="inherit" className={classes.effectIcon} />{' '}
-                                <Typography variant="subtitle2">Transition</Typography>
-                              </Effect>
-                            </div>
-                            {snapshot.isDragging && (
-                              <Effect>
-                                <MovieFilterIcon fontSize="small" color="inherit" className={classes.effectIcon} />{' '}
-                                <Typography variant="subtitle2">Transition</Typography>
-                              </Effect>
-                            )}
-                          </>
-                        )}
-                      </DraggableItem>
-                    </Grid>
+                            </Tooltip>
+                          </div>
+                          {snapshot.isDragging && (
+                            <Effect>
+                              <MovieFilterIcon color="primary" fontSize="small" className={classes.effectIcon} />{' '}
+                              <Typography variant="subtitle2" color="primary">
+                                Transition
+                              </Typography>
+                            </Effect>
+                          )}
+                        </>
+                      )}
+                    </DraggableItem>
                   </Grid>
                 </Grid>
-              </Toolbar>
-            </div>
-          )}
-        </Droppable>
-      </Root>
-    </Draggable>
+              </Grid>
+            </Toolbar>
+          </div>
+        )}
+      </Droppable>
+    </Root>
   );
 };
