571e88e78eca0202e87b7bdc796662b767bf7cd7
Piotr F <hello@piotrf.pl>
2021-11-14 23:18:32 +0100

    Copy.

--- packages/remixer/src/components/Transcript.js
+++ packages/remixer/src/components/Transcript.js
@@ -255,7 +255,7 @@ export const Transcript = props => {
           <ListItemIcon>
             <DeleteIcon color="error" fontSize="small" />
           </ListItemIcon>
-          <ListItemText primary="Remove" primaryTypographyProps={{ color: 'error' }} />
+          <ListItemText primary="Remove section" primaryTypographyProps={{ color: 'error' }} />
         </MenuItem>
       </Menu>
     </Root>
