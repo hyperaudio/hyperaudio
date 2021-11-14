0c55238fc6f7b67d2d590736f80e4279a72aab5a
Piotr F <hello@piotrf.pl>
2021-11-15 00:27:55 +0100

    Document title insert.

--- packages/remixer/src/components/InsertTitle.js
+++ packages/remixer/src/components/InsertTitle.js
@@ -1,5 +1,106 @@
 import React from 'react';
 
+import Paper from '@mui/material/Paper';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import { styled } from '@mui/material/styles';
+
+import { FullSizeIcon, LowerThirdsIcon } from '../icons';
+
+const PREFIX = 'InsertTitle';
+const classes = {
+  icon: `${PREFIX}-icon`,
+  field: `${PREFIX}-field`,
+};
+
+const Root = styled(Paper)(({ theme }) => ({
+  borderRadius: theme.shape.borderRadius,
+  padding: theme.spacing(1),
+}));
+
+const Canvas = styled('div', {
+  shouldForwardProp: prop => prop !== 'fullSize',
+})(({ theme, fullSize }) => ({
+  background: theme.palette.text.primary,
+  color: theme.palette.primary.contrastText,
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: fullSize ? 'center' : 'flex-end',
+  minHeight: '120px',
+  padding: theme.spacing(2),
+  textAlign: 'center',
+  [`& .${classes.field}`]: {
+    [`& .MuiOutlinedInput-notchedOutline`]: {
+      border: 'none',
+    },
+    input: {
+      ...theme.typography.h6,
+      border: 'none',
+      color: theme.palette.primary.contrastText,
+      textAlign: 'center',
+    },
+  },
+}));
+
+const Controls = styled('div')(({ theme }) => ({
+  color: theme.palette.text.disabled,
+  borderRadius: theme.shape.borderRadius,
+  padding: theme.spacing(1),
+  [`& .${classes.icon}`]: {
+    fontSize: '16px',
+    lineHeight: 0,
+    verticalAlign: 'middle',
+    marginRight: theme.spacing(0.5),
+  },
+}));
+
+const Control = styled('a', {
+  shouldForwardProp: prop => prop !== 'isActive',
+})(({ theme, isActive }) => ({
+  color: isActive ? 'inherit' : theme.palette.primary.dark,
+  cursor: 'pointer',
+  display: 'inline-block',
+  [`&:hover`]: {
+    color: theme.palette.primary.main,
+  },
+}));
+
 export const InsertTitle = props => {
-  return <div>Title</div>;
+  const { fullSize, text, onTextChange, onSetFullSize } = props;
+
+  const [titleText, setTitleText] = React.useState(text);
+
+  return (
+    <Root>
+      <Canvas fullSize={fullSize}>
+        <TextField
+          className={classes.field}
+          onBlur={e => onTextChange(e.target.value)}
+          onChange={e => setTitleText(e.target.value)}
+          size="small"
+          value={titleText}
+        />
+      </Canvas>
+      <Controls>
+        <Control isActive={fullSize} onClick={() => onSetFullSize(true)}>
+          <FullSizeIcon className={classes.icon} />
+          <Typography variant="caption" underline="hover">
+            Full-size
+          </Typography>
+        </Control>{' '}
+        ⋅ 
+        <Control isActive={!fullSize} onClick={() => onSetFullSize(false)}>
+          <LowerThirdsIcon className={classes.icon} />
+          <Typography variant="caption" underline="hover">
+            Lower-thirds
+          </Typography>
+        </Control>
+      </Controls>
+    </Root>
+  );
+};
+
+InsertTitle.defaultProps = {
+  text: 'Type in your title here…',
+  fullSize: true,
 };
--- packages/storybook/stories/RemixerComponents.stories.jsx
+++ packages/storybook/stories/RemixerComponents.stories.jsx
@@ -7,12 +7,28 @@ import {
 } from "../../remixer/src/components";
 
 export default {
-  title: "Packages/Remix Insert",
+  title: "Packages/Remix Inserts",
 };
 
-const InsertTitleTpl = (args) => <InsertTitle {...args} />;
-export const Title = InsertTitleTpl.bind({});
-Title.args = {};
+const InsertTitleTpl = (args) => (
+  <InsertTitle
+    onTextChange={(str) => console.log("onTextChange:", str)}
+    onSetFullSize={(bool) => console.log("onSetFullSize:", bool)}
+    {...args}
+  />
+);
+export const EmptyTitle = InsertTitleTpl.bind({});
+EmptyTitle.args = {};
+export const FullSizeTitle = InsertTitleTpl.bind({});
+FullSizeTitle.args = {
+  text: "This is a full-size title",
+  fullSize: true,
+};
+export const LowerThirdsTitle = InsertTitleTpl.bind({});
+LowerThirdsTitle.args = {
+  text: "This is a lower-thirds title",
+  fullSize: false,
+};
 
 const InsertSlideTpl = (args) => <InsertSlide {...args} />;
 export const Slide = InsertSlideTpl.bind({});
