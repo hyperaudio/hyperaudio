5b751d39734e780d5c974c551a2b0ad66dae5220
Laurian Gridinoc <laurian@gridinoc.name>
2022-01-31 20:47:30 +0200

    account update

--- conferences/src/pages/account/index.js
+++ conferences/src/pages/account/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useEffect } from 'react';
+import React, { useState, useCallback, useEffect, useReducer } from 'react';
 import { Auth, DataStore, syncExpression, withSSRContext } from 'aws-amplify';
 import { serializeModel, deserializeModel } from '@aws-amplify/datastore/ssr';
 import { NoSsr } from '@mui/base';
@@ -7,7 +7,7 @@ import { AccountView } from '@hyperaudio/app';
 
 import { User } from '../../models';
 
-const getUser = async (setUser, identityId) => {
+const getUser = async (dispatch, identityId) => {
   DataStore.configure({
     syncExpressions: [
       syncExpression(User, () => {
@@ -17,17 +17,42 @@ const getUser = async (setUser, identityId) => {
   });
 
   const user = await DataStore.query(User, user => user.identityId('eq', identityId), { limit: 1 });
-  setUser(Array.isArray(user) ? user[0] : user);
+  dispatch({ type: 'loadUser', payload: Array.isArray(user) ? user[0] : user });
+};
+
+export const userReducer = (state, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case 'loadUser':
+      return { user: payload };
+    case 'updateName':
+      return { ...state, name: payload };
+    case 'updateBio':
+      return { ...state, bio: payload };
+    case 'save':
+      DataStore.save(
+        User.copyOf(state.user, updated => {
+          updated.name = state.name ?? updated.name;
+          updated.bio = state.bio ?? updated.bio;
+        }),
+      );
+      return state;
+    default:
+      throw new Error(`unhandled action ${type}`, action);
+  }
 };
 
 const AccountPage = initialData => {
   const { identityId } = initialData;
-  const [user, setUser] = useState(initialData.user ? deserializeModel(User, initialData.user) : null);
+  const [{ user, name, bio }, dispatch] = useReducer(userReducer, {
+    user: initialData.user ? deserializeModel(User, initialData.user) : null,
+  });
 
-  useEffect(() => identityId && getUser(setUser, identityId), [identityId]);
+  useEffect(() => identityId && getUser(dispatch, identityId), [identityId]);
 
   useEffect(() => {
-    const subscription = DataStore.observe(User).subscribe(() => identityId && getUser(setUser, identityId));
+    const subscription = DataStore.observe(User).subscribe(() => identityId && getUser(dispatch, identityId));
     return () => subscription.unsubscribe();
   }, [identityId]);
 
@@ -36,9 +61,10 @@ const AccountPage = initialData => {
       <NoSsr>
         <AccountView
           account={{
-            displayName: user?.name ?? '',
-            bio: user?.bio ?? '',
+            name: name ?? user?.name ?? '',
+            bio: bio ?? user?.bio ?? '',
           }}
+          dispatch={dispatch}
         />
       </NoSsr>
       <pre>{JSON.stringify(user, null, 2)}</pre>
