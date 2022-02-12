9e16e9f84bdf1df441fe4fe1da2c7742ab7d03e2
Piotr F <hello@piotrf.pl>
2022-02-12 01:23:37 +0100

    Fix media page scrolling.

--- conferences/src/pages/media/[slug]/index.js
+++ conferences/src/pages/media/[slug]/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 
+import { styled } from '@mui/material/styles';
+
 import Remixer from '@hyperaudio/remixer';
 
 import { remixData } from './data';
+
+const PREFIX = 'MediaPage';
+const classes = {
+  root: `${PREFIX}-root`,
+};
+
+const Root = styled('div', {
+  // shouldForwardProp: (prop: any) => prop !== 'isActive',
+})(({ theme }) => ({
+  bottom: 0,
+  left: 0,
+  position: 'absolute',
+  right: 0,
+  top: 0,
+}));
 
 const MediaPage = () => {
   const router = useRouter();
@@ -14,7 +31,15 @@ const MediaPage = () => {
   console.groupEnd();
 
   return (
-    <Remixer editable={false} isSingleMedia={true} media={remixData.sources} remix={null} sources={remixData.sources} />
+    <Root className={classes.root}>
+      <Remixer
+        editable={false}
+        isSingleMedia={true}
+        media={remixData.sources}
+        remix={null}
+        sources={remixData.sources}
+      />
+    </Root>
   );
 };
 
