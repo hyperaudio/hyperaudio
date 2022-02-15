28cd87a771a4c18fa8d0ee4430816808d40948bc
Piotr F <hello@piotrf.pl>
2022-02-15 16:45:48 +0100

    Push hardcoded home page.

--- conferences/src/pages/index.js
+++ conferences/src/pages/index.js
@@ -38,11 +38,271 @@ const Root = styled(
 const HomePage = props => {
   console.log(props);
 
-  //  const channels = appData.channels.map(channel => {
-  //    const media = _.filter(appData.media, o => o.channelId === channel.channelId);
-  //    return { ...channel, media };
-  //  });
-  const channels = [];
+  const media = [
+    {
+      mediaId: 0,
+      name: 'The Future of Podcasting is Adaptive, Open and Data ethical I',
+      description: 'Dolor eiusmod quis non esse nulla quis amet ad nostrud non mollit quis culpa.',
+      thumb: 'https://picsum.photos/400/225',
+      created: '2002-04-02',
+      modified: null,
+      status: 'uploading',
+      isPublic: true,
+      channelId: null,
+    },
+    {
+      mediaId: 1,
+      name: 'Podcasting has no future.',
+      description: 'Dolor eiusmod quis non esse nulla quis amet ad nostrud non mollit quis culpa.',
+      thumb: 'https://picsum.photos/400/225',
+      created: '2002-04-02',
+      modified: null,
+      status: 'uploaded',
+      isPublic: false,
+      channelId: null,
+    },
+    {
+      mediaId: 2,
+      name: 'I wish to see yet another media title.',
+      description: 'Dolor eiusmod quis non esse nulla quis amet ad nostrud non mollit quis culpa.',
+      thumb: 'https://picsum.photos/400/225',
+      created: '2002-04-02',
+      modified: '2011-02-02',
+      status: 'transcribing',
+      isPublic: true,
+      channelId: 1,
+    },
+    {
+      mediaId: 3,
+      name: 'The Future of Podcasting is Adaptive, Open and Data ethical IV',
+      description: 'Dolor eiusmod quis non esse nulla quis amet ad nostrud non mollit quis culpa.',
+      thumb: 'https://picsum.photos/400/225',
+      created: '2002-04-02',
+      modified: '2011-02-18',
+      status: 'transcribed',
+      isPublic: false,
+      channelId: 1,
+    },
+    {
+      mediaId: 4,
+      name: 'The Future of Podcasting is Adaptive, Open and Data ethical V',
+      description: 'Dolor eiusmod quis non esse nulla quis amet ad nostrud non mollit quis culpa.',
+      thumb: 'https://picsum.photos/400/225',
+      created: '2002-02-01',
+      modified: '2011-02-17',
+      status: 'edited',
+      isPublic: true,
+      channelId: 0,
+    },
+    {
+      mediaId: 5,
+      name: 'What’s next for online conferences?',
+      description: 'Dolor eiusmod quis non esse nulla quis amet ad nostrud non mollit quis culpa.',
+      thumb: 'https://picsum.photos/400/225',
+      created: '2001-11-12',
+      modified: '2011-02-27',
+      status: 'corrected',
+      isPublic: false,
+      channelId: 1,
+    },
+    {
+      mediaId: 6,
+      name: 'How about a short title?',
+      description: 'Dolor eiusmod quis non esse nulla quis amet ad nostrud non mollit quis culpa.',
+      thumb: 'https://picsum.photos/400/225',
+      created: '2001-10-29',
+      modified: '2011-01-22',
+      status: 'ready',
+      isPublic: true,
+      channelId: 0,
+    },
+    {
+      mediaId: 7,
+      name: 'We’d love to see some other title so all doesn’t just blend',
+      description: 'Dolor eiusmod quis non esse nulla quis amet ad nostrud non mollit quis culpa.',
+      thumb: 'https://picsum.photos/400/225',
+      created: '2001-09-26',
+      modified: '2011-01-21',
+      status: 'ready',
+      isPublic: false,
+      channelId: 1,
+    },
+    {
+      mediaId: 8,
+      name: 'The Future of Podcasting is Adaptive, Open and Data ethical',
+      description: 'Dolor eiusmod quis non esse nulla quis amet ad nostrud non mollit quis culpa.',
+      thumb: 'https://picsum.photos/400/225',
+      created: '2001-07-11',
+      modified: '2011-01-10',
+      status: 'ready',
+      isPublic: true,
+      channelId: 1,
+    },
+    {
+      mediaId: 9,
+      name: 'The Future of Podcasting is Adaptive, Open and Data ethical',
+      description: 'Dolor eiusmod quis non esse nulla quis amet ad nostrud non mollit quis culpa.',
+      thumb: 'https://picsum.photos/400/225',
+      created: '2001-06-13',
+      modified: '2001-07-01',
+      status: 'ready',
+      isPublic: false,
+      channelId: 0,
+    },
+    {
+      mediaId: 10,
+      name: 'The Future of Podcasting is Adaptive, Open and Data ethical',
+      description: 'Dolor eiusmod quis non esse nulla quis amet ad nostrud non mollit quis culpa.',
+      thumb: 'https://picsum.photos/400/225',
+      created: '2001-04-23',
+      modified: '2011-01-05',
+      status: 'error',
+      isPublic: true,
+      channelId: 1,
+    },
+    {
+      mediaId: 11,
+      name: 'The Future of Podcasting is Adaptive, Open and Data ethical I',
+      description: 'Dolor eiusmod quis non esse nulla quis amet ad nostrud non mollit quis culpa.',
+      thumb: 'https://picsum.photos/400/225',
+      created: '2002-04-02',
+      modified: null,
+      status: 'uploading',
+      isPublic: true,
+      channelId: 0,
+    },
+    {
+      mediaId: 12,
+      name: 'Podcasting has no future.',
+      description: 'Dolor eiusmod quis non esse nulla quis amet ad nostrud non mollit quis culpa.',
+      thumb: 'https://picsum.photos/400/225',
+      created: '2002-04-02',
+      modified: null,
+      status: 'uploaded',
+      isPublic: false,
+      channelId: 1,
+    },
+    {
+      mediaId: 13,
+      name: 'I wish to see yet another media title.',
+      description: 'Dolor eiusmod quis non esse nulla quis amet ad nostrud non mollit quis culpa.',
+      thumb: 'https://picsum.photos/400/225',
+      created: '2002-04-02',
+      modified: '2011-02-02',
+      status: 'transcribing',
+      isPublic: true,
+      channelId: 1,
+    },
+    {
+      mediaId: 14,
+      name: 'The Future of Podcasting is Adaptive, Open and Data ethical IV',
+      description: 'Dolor eiusmod quis non esse nulla quis amet ad nostrud non mollit quis culpa.',
+      thumb: 'https://picsum.photos/400/225',
+      created: '2002-04-02',
+      modified: '2011-02-18',
+      status: 'transcribed',
+      isPublic: false,
+      channelId: 0,
+    },
+    {
+      mediaId: 15,
+      name: 'The Future of Podcasting is Adaptive, Open and Data ethical V',
+      description: 'Dolor eiusmod quis non esse nulla quis amet ad nostrud non mollit quis culpa.',
+      thumb: 'https://picsum.photos/400/225',
+      created: '2002-02-01',
+      modified: '2011-02-17',
+      status: 'edited',
+      isPublic: true,
+      channelId: 0,
+    },
+    {
+      mediaId: 16,
+      name: 'What’s next for online conferences?',
+      description: 'Dolor eiusmod quis non esse nulla quis amet ad nostrud non mollit quis culpa.',
+      thumb: 'https://picsum.photos/400/225',
+      created: '2001-11-12',
+      modified: '2011-02-27',
+      status: 'corrected',
+      isPublic: false,
+      channelId: 1,
+    },
+    {
+      mediaId: 17,
+      name: 'How about a short title?',
+      description: 'Dolor eiusmod quis non esse nulla quis amet ad nostrud non mollit quis culpa.',
+      thumb: 'https://picsum.photos/400/225',
+      created: '2001-10-29',
+      modified: '2011-01-22',
+      status: 'ready',
+      isPublic: true,
+      channelId: 1,
+    },
+    {
+      mediaId: 18,
+      name: 'We’d love to see some other title so all doesn’t just blend',
+      description: 'Dolor eiusmod quis non esse nulla quis amet ad nostrud non mollit quis culpa.',
+      thumb: 'https://picsum.photos/400/225',
+      created: '2001-09-26',
+      modified: '2011-01-21',
+      status: 'ready',
+      isPublic: false,
+      channelId: 1,
+    },
+    {
+      mediaId: 19,
+      name: 'The Future of Podcasting is Adaptive, Open and Data ethical',
+      description: 'Dolor eiusmod quis non esse nulla quis amet ad nostrud non mollit quis culpa.',
+      thumb: 'https://picsum.photos/400/225',
+      created: '2001-07-11',
+      modified: '2011-01-10',
+      status: 'ready',
+      isPublic: true,
+      channelId: 1,
+    },
+    {
+      mediaId: 20,
+      name: 'The Future of Podcasting is Adaptive, Open and Data ethical',
+      description: 'Dolor eiusmod quis non esse nulla quis amet ad nostrud non mollit quis culpa.',
+      thumb: 'https://picsum.photos/400/225',
+      created: '2001-06-13',
+      modified: '2001-07-01',
+      status: 'ready',
+      isPublic: false,
+      channelId: 0,
+    },
+    {
+      mediaId: 21,
+      name: 'The Future of Podcasting is Adaptive, Open and Data ethical',
+      description: 'Dolor eiusmod quis non esse nulla quis amet ad nostrud non mollit quis culpa.',
+      thumb: 'https://picsum.photos/400/225',
+      created: '2001-04-23',
+      modified: '2011-01-05',
+      status: 'error',
+      isPublic: true,
+      channelId: null,
+    },
+  ];
+
+  const channels = [
+    {
+      channelId: 0,
+      name: 'Creative AI',
+      description:
+        'Can complex code be written creatively? Does art emerge from AI algorithms? How can we teach AI— or about AI—creatively? At MozFest you will have plenty of room to explore these questions in the Creative AI space! By fuelling a community that uses creativity to re-envision, question, and interact with AI and its effects on our daily lives, we will co-create a better future where humans and machines collaborate to unleash the best of us. Collaborative art making, hands-on learning, open studio sessions, critical reflection, forward facing discussions, web-native exhibitions and much more await you in the Creative AI space!',
+    },
+    {
+      channelId: 1,
+      name: 'AI Wellness',
+      description:
+        'AI Wellness is a transformative space changing artificial intelligence into authentic intelligence. This Space will channel community knowledge and experiences with technology to inform the future of human-centered AI that benefits society and individuals. If you are a dreamer, innovator, artist, technologist, storyteller, healer, or defender of healthy online communities, please join us. We invite you to contribute by sharing stories about how AI shapes our lives, by demystifying technology with understanding, and by creating community-centered solutions for healthier AI. Together, we will envision AI that promotes joy, healing, and wellbeing for all.',
+    },
+  ];
+
+  const groups = channels.map(channel => {
+    const items = _.filter(media, o => o.channelId === channel.channelId);
+    return { ...channel, media: items };
+  });
+  // const channels = [];
 
   return (
     <Root className={classes.root}>
@@ -52,25 +312,23 @@ const HomePage = props => {
       </Head>
 
       <Main>
-        <h1>Hyperaudio</h1>
-
-        {channels.map(channel => {
+        {groups.map(group => {
           return [
-            <Container maxWidth={false} key={channel.channelId}>
-              <Grid container key={`g-${channel.channelId}`} spacing={{ xs: 4, md: 8 }}>
-                <Grid item xs={12} md={4} xl={3}>
+            <Container maxWidth={false} key={group.channelId}>
+              <Grid container key={`g-${group.channelId}`} spacing={{ xs: 4, md: 8 }}>
+                <Grid item xs={12} md={4} xl={4}>
                   <Typography variant="h5" component="h1" gutterBottom>
-                    {channel.name}
+                    {group.name}
                   </Typography>
                   <Typography variant="body2" component="p">
-                    {channel.description}
+                    {group.description}
                   </Typography>
                 </Grid>
-                <Grid item xs={12} md={8} xl={9}>
+                <Grid item xs={12} md={8} xl={8}>
                   <Grid container spacing={4}>
-                    {channel.media.map(media => {
+                    {group.media.map(media => {
                       return (
-                        <Grid item key={media.mediaId} lg={3} md={4} sm={4} xl={2} xs={6}>
+                        <Grid item key={media.mediaId} xs={6} sm={4}>
                           <Card sx={{ mb: 1 }}>
                             <CardActionArea onClick={() => console.log('onMediaOpen', media.mediaId)}>
                               <CardMedia component="img" height="100%" image={media.thumb} />
@@ -95,7 +353,7 @@ const HomePage = props => {
                 </Grid>
               </Grid>
             </Container>,
-            <Divider key={`d-${channel.channelId}`} light sx={{ mt: 8, mb: 8 }} variant="fullWidth" />,
+            <Divider key={`d-${group.channelId}`} light sx={{ mt: 8, mb: 8 }} variant="fullWidth" />,
           ];
         })}
       </Main>
