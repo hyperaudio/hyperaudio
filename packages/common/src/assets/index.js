7a2a6677e58dee5877bdebc46e9dcadd55e53430
Piotr F <hello@piotrf.pl>
2022-02-22 15:53:01 +0100

    Bring in all Hyperaudio logos inside of Common.

--- packages/common/src/assets/index.js
+++ packages/common/src/assets/index.js
@@ -1,1 +1,5 @@
+export { default as HyperaudioConferences } from './HyperaudioConferences';
+export { default as HyperaudioMain } from './HyperaudioMain';
+export { default as HyperaudioPodcasts } from './HyperaudioPodcasts';
+export { default as HyperaudioSchools } from './HyperaudioSchools';
 export { default as Brandmark } from './Brandmark';
