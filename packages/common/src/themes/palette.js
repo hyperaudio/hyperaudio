f02439a3c600cef1032c2d3f199e7ccd28e27cb8
Piotr F <hello@piotrf.pl>
2022-03-24 20:02:00 +0100

    Change secondary color to teal.

--- packages/common/src/themes/palette.js
+++ packages/common/src/themes/palette.js
@@ -1,5 +1,5 @@
 import { createTheme } from '@mui/material/styles';
-import { amber, deepPurple, grey } from '@mui/material/colors';
+import { teal, deepPurple, grey } from '@mui/material/colors';
 
 const mui = createTheme();
 
@@ -18,7 +18,7 @@ const palette = {
   },
   divider: 'rgba(0,0,0,0.088)',
   primary: deepPurple,
-  secondary: amber,
+  secondary: teal,
 };
 
 export default palette;
