1aaa6fe5e5b62e0f262b948f8e379ec2b5e18895
Laurian Gridinoc <laurian@gridinoc.name>
2022-03-28 16:28:36 +0200

    auth fixes

--- conferences/src/pages/auth/logout.js
+++ conferences/src/pages/auth/logout.js
@@ -1,10 +1,14 @@
 import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import { Auth } from 'aws-amplify';
 
 const Logout = () => {
+  const router = useRouter();
+
   useEffect(() => {
     (async () => {
       await Auth.signOut({ global: true });
+      setTimeout(() => router.push('/'), 1000);
     })();
   });
 
