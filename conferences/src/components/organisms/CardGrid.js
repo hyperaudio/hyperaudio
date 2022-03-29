9ee1e1b30586b030d35641482675ed5549ec13df
Piotr F <hello@piotrf.pl>
2022-03-30 01:45:27 +0200

    Cleanup home page.

--- conferences/src/components/organisms/CardGrid.js
+++ conferences/src/components/organisms/CardGrid.js
@@ -10,62 +10,140 @@ import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import { styled } from '@mui/material/styles';
 
+import { HyperaudioIcon } from '@hyperaudio/common';
+
 import Link from '../MuiNextLink';
 
 const PREFIX = `CardGrid`;
 const classes = {
+  actionArea: `${PREFIX}-actionArea`,
   card: `${PREFIX}-card`,
-  cardActionArea: `${PREFIX}-cardActionArea`,
+  coverCard: `${PREFIX}-coverCard`,
+  coverContent: `${PREFIX}-coverContent`,
+  coverOrnament: `${PREFIX}-coverOrnament`,
   root: `${PREFIX}-root`,
-  titleTile: `${PREFIX}-titleTile`,
 };
 const Root = styled(Box)(({ theme }) => ({
-  [`& .${classes.titleTile}`]: {
-    // backgroundImage: `linear-gradient(to bottom, ${theme.palette.primary.dark} -50%, ${theme.palette.secondary.dark} 150%)`,
-    background: theme.palette.primary.dark,
+  [`& .${classes.coverCard}`]: {
+    backgroundImage: `linear-gradient(to bottom, ${theme.palette.primary.dark} -50%, ${theme.palette.secondary.dark} 150%)`,
+    borderRadius: 0,
     color: theme.palette.primary.contrastText,
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100%',
+    justifyContent: 'center',
+    minHeight: '25vh',
+    overflow: 'hidden',
+    position: 'relative',
+    [theme.breakpoints.up('md')]: {
+      borderRadius: theme.shape.borderRadius * 2,
+      boxShadow: theme.shadows[12],
+      minHeight: 'auto',
+    },
+    [theme.breakpoints.up('lg')]: {
+      boxShadow: theme.shadows[24],
+      justifyContent: 'flex-start',
+      transform: 'scale(1.05)',
+    },
+  },
+  [`& .${classes.coverContent}`]: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
     overflow: 'auto',
+    [theme.breakpoints.up('md')]: {
+      bottom: 0,
+      justifyContent: 'flex-start',
+      left: 0,
+      position: 'absolute',
+      right: 0,
+      top: 0,
+    },
+  },
+  [`& .${classes.coverOrnament}`]: {
+    bottom: 0,
+    fontSize: '444px',
+    opacity: 0.05,
+    pointerEvents: 'none',
+    position: 'absolute',
+    right: 0,
+    transform: 'translate(33%, 66%) rotate(202deg) ',
+  },
+  [`& .${classes.card}`]: {
+    height: '100%',
+    boxShadow: theme.shadows[6],
+  },
+  [`& .${classes.actionArea}`]: {
+    position: 'relative',
+    '&:after': {
+      backgroundImage: `linear-gradient(to bottom, ${theme.palette.primary.dark} -50%, ${theme.palette.secondary.dark} 150%)`,
+      bottom: 0,
+      content: "' '",
+      left: 0,
+      opacity: 0.22,
+      position: 'absolute',
+      right: 0,
+      top: 0,
+      transition: `opacity ${theme.transitions.duration.standard}ms`,
+    },
+    '&:hover:after': {
+      opacity: 0,
+    },
   },
 }));
 
 const CardGrid = props => {
-  const { featured, title, text, items, onItemTouch } = props;
+  const { title, text, items } = props;
   console.log({ items });
+
   return (
-    <Root className={classes.root} sx={{ mx: { xs: -2, sm: -3, lg: 0 } }}>
-      <Grid container spacing={{ xs: 0, lg: 6 }} alignContent="stretch">
-        <Grid item xs={12} sm={6} md={4}>
-          <Paper
-            elevation={24}
-            className={classes.titleTile}
-            sx={{ height: '100%', borderRadius: { xs: 0, lg: 2 }, p: { xs: 3, md: 4 } }}
-          >
-            <Typography component="h3" display="block" gutterBottom variant="h2">
-              {title}
-            </Typography>
-            <Typography variant="body2" color="inherit" display="block" component="div">
-              <ReactMarkdown>{text}</ReactMarkdown>
-            </Typography>
+    <Root className={classes.root}>
+      <Grid container spacing={{ xs: 2, sm: 3, md: 4, lg: 6 }}>
+        <Grid item xs={12} md={4} lg={4} xl={3}>
+          <Paper className={classes.coverCard} sx={{ mx: { xs: -2, sm: -3, md: 0 } }}>
+            <Box className={classes.coverContent} sx={{ pt: { xs: 4, md: 4, lg: 6 }, px: { xs: 2, sm: 3, md: 4 } }}>
+              <Typography component="h2" display="block" gutterBottom variant="h1" sx={{ overflowWrap: 'break-word' }}>
+                {title}
+              </Typography>
+              <Typography variant="body1" color="inherit" display="block" component="div">
+                <ReactMarkdown>{text}</ReactMarkdown>
+              </Typography>
+            </Box>
+            <HyperaudioIcon className={classes.coverOrnament} color="white" />
           </Paper>
         </Grid>
         {items.map(item => (
-          <Grid item key={item.id} xs={6} sm={6} md={4}>
-            <Card
-              elevation={6}
-              key={item.id}
-              sx={{
-                borderRadius: { xs: 0, lg: 2 },
-                display: 'flex',
-                flexDirection: 'column',
-                height: '100%',
-                jusitfyContent: 'flex-start',
-              }}
-            >
-              <CardActionArea component={Link} href={`/media/${item.id}`}>
-                <CardMedia component="img" image={item.poster} alt={item.title} />
-              </CardActionArea>
-              <CardContent sx={{ p: { xs: 3, sm: 4 }, maxHeight: '300px', overflow: 'auto', flex: '0 0 100%' }}>
-                <Link href={`/media/${item.id}`} variant="subtitle2">
+          <Grid item key={item.id} xs={6} md={4} lg={4} xl={3}>
+            <Card className={classes.card} key={item.id} sx={{ borderRadius: 2 }}>
+              <Box sx={{ flex: '0 0 auto', p: { xs: 2, lg: 0 } }}>
+                <CardActionArea component={Link} href={`/media/${item.id}`} className={classes.actionArea}>
+                  <CardMedia
+                    component="img"
+                    image={item.poster}
+                    alt={item.title}
+                    sx={{
+                      alignContent: 'center',
+                      aspectRatio: `16 / 9`,
+                      bgcolor: 'primary',
+                      borderRadius: { xs: 1, lg: 0 },
+                      display: 'flex',
+                      flexDirection: 'column',
+                      justifyContent: 'center',
+                    }}
+                  />
+                </CardActionArea>
+              </Box>
+              <CardContent
+                sx={{
+                  flex: '0 0 100%',
+                  maxHeight: '260px',
+                  overflow: 'auto',
+                  pb: { xs: 2, lg: 0 },
+                  pt: { xs: 0, lg: 4 },
+                  px: { xs: 2, lg: 4 },
+                }}
+              >
+                <Link href={`/media/${item.id}`} variant="subtitle2" sx={{ overflowWrap: 'break-word' }}>
                   {item.title}
                 </Link>
                 <Typography
