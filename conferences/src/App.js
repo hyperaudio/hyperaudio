a912917e4e0f85ef1fb3fc5c435269c8a8c77404
Piotr F <hello@piotrf.pl>
2022-04-10 18:54:51 +0200

    Hide footer if user is null.

--- conferences/src/App.js
+++ conferences/src/App.js
@@ -152,6 +152,8 @@ const App = props => {
   const [domain, setDomain] = useState(global?.location?.hostname);
   const [supportsIndexedDB, setSupportsIndexedDB] = useState(true);
 
+  console.log({ user });
+
   useEffect(
     () =>
       (async () => {
@@ -267,41 +269,45 @@ const App = props => {
                 <HyperaudioIcon />
               </Fab>
             </Tooltip>
-            <Slide appear={false} direction="up" in={!trigger}>
-              <Box className={classes.foot} sx={{ left: 0, borderWidth: '1px 1px 0 0' }}>
-                <Stack direction="row" spacing={1}>
-                  <Typography color="textSecondary" variant="caption" sx={{ fontSize: '12px' }}>
-                    ©{' '}
-                    <Link {...linkProps} href="https://hyper.audio">
-                      The Hyperaudio Project
-                    </Link>
-                  </Typography>
-                </Stack>
-              </Box>
-            </Slide>
-            <Slide appear={false} direction="up" in={!trigger}>
-              <Box className={classes.foot} sx={{ right: 0, borderWidth: '1px 0 0 1px' }}>
-                <Stack direction="row" spacing={2}>
-                  <Link {...linkProps} href="https://hyper.audio/terms-of-service">
-                    Terms of Service
-                  </Link>
-                  <Link
-                    {...linkProps}
-                    href="https://hyper.audio/privacy-policy"
-                    sx={{ ...linkProps.sx, display: { xs: 'none', sm: 'inline' } }}
-                  >
-                    Privacy Policy
-                  </Link>
-                  <Link
-                    {...linkProps}
-                    href="https://hyper.audio/code-of-conduct"
-                    sx={{ ...linkProps.sx, display: { xs: 'none', md: 'inline' } }}
-                  >
-                    Code of Conduct
-                  </Link>
-                </Stack>
-              </Box>
-            </Slide>
+            {user === null && (
+              <>
+                <Slide appear={false} direction="up" in={!trigger}>
+                  <Box className={classes.foot} sx={{ left: 0, borderWidth: '1px 1px 0 0' }}>
+                    <Stack direction="row" spacing={1}>
+                      <Typography color="textSecondary" variant="caption" sx={{ fontSize: '12px' }}>
+                        ©{' '}
+                        <Link {...linkProps} href="https://hyper.audio">
+                          The Hyperaudio Project
+                        </Link>
+                      </Typography>
+                    </Stack>
+                  </Box>
+                </Slide>
+                <Slide appear={false} direction="up" in={!trigger}>
+                  <Box className={classes.foot} sx={{ right: 0, borderWidth: '1px 0 0 1px' }}>
+                    <Stack direction="row" spacing={2}>
+                      <Link {...linkProps} href="https://hyper.audio/terms-of-service">
+                        Terms of Service
+                      </Link>
+                      <Link
+                        {...linkProps}
+                        href="https://hyper.audio/privacy-policy"
+                        sx={{ ...linkProps.sx, display: { xs: 'none', sm: 'inline' } }}
+                      >
+                        Privacy Policy
+                      </Link>
+                      <Link
+                        {...linkProps}
+                        href="https://hyper.audio/code-of-conduct"
+                        sx={{ ...linkProps.sx, display: { xs: 'none', md: 'inline' } }}
+                      >
+                        Code of Conduct
+                      </Link>
+                    </Stack>
+                  </Box>
+                </Slide>
+              </>
+            )}
           </Root>
         </PlausibleProvider>
       </ThemeProvider>
