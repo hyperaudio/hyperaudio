aee8bd3ffd06cc58771ab95b83b91677b62a224b
Laurian Gridinoc <laurian@gridinoc.name>
2022-04-19 21:24:58 +0200

    COC fix

--- website/src/pages/code-of-conduct.tsx
+++ website/src/pages/code-of-conduct.tsx
@@ -25,7 +25,10 @@ const CodeOfConduct: NextPage = () => (
         Introduction
       </Typography>
       <Typography variant="body2" gutterBottom>
-        The Hyperaudio Team is committed to building a safe, welcoming, harassment-free culture for everyone. We not only strive for an environment that is free from hostility; but also one that is actively welcoming and inclusive. By celebrating diversity, we believe that together we can build a stronger and more welcoming community.
+        The Hyperaudio Team is committed to building a safe, welcoming, harassment-free culture for everyone. We not
+        only strive for an environment that is free from hostility; but also one that is actively welcoming and
+        inclusive. By celebrating diversity, we believe that together we can build a stronger and more welcoming
+        community.
       </Typography>
       <Typography variant="body2" gutterBottom>
         The Hyperaudio community is active in a number of social spaces:
@@ -56,7 +59,8 @@ const CodeOfConduct: NextPage = () => (
         And on the Hyperaudio Platform itself.
       </Typography>
       <Typography variant="body2" gutterBottom>
-        What follows is a description of our values and behaviour that we hope will ensure vibrant and safe places in which to express ourselves and be creative.
+        What follows is a description of our values and behaviour that we hope will ensure vibrant and safe places in
+        which to express ourselves and be creative.
       </Typography>
       <Typography variant="body2" gutterBottom>
         We’ll also outline what you can do if you feel that behaviour does not fit with our or even your* values.
@@ -68,7 +72,9 @@ const CodeOfConduct: NextPage = () => (
         Values
       </Typography>
       <Typography variant="body2" gutterBottom>
-        We strive to create a welcoming and inclusive culture that empowers and encourages people to be collaboratively creative, whether writing code or mixing media. The atmosphere we seek requires an open exchange of ideas balanced by thoughtful guidelines:
+        We strive to create a welcoming and inclusive culture that empowers and encourages people to be collaboratively
+        creative, whether writing code or mixing media. The atmosphere we seek requires an open exchange of ideas
+        balanced by thoughtful guidelines:
       </Typography>
       <ul>
         <li>
@@ -103,17 +109,20 @@ const CodeOfConduct: NextPage = () => (
         </li>
         <li>
           <Typography variant="body2" gutterBottom>
-            Treat other people’s identities and cultures with respect. Make an effort to say or spell people’s names correctly and refer to them by their chosen pronouns.
+            Treat other people’s identities and cultures with respect. Make an effort to say or spell people’s names
+            correctly and refer to them by their chosen pronouns.
           </Typography>
         </li>
         <li>
           <Typography variant="body2" gutterBottom>
-            Strive to build tools that are open and free technology for public use. Activities that aim to foster public use, not private gain, are prioritised.
+            Strive to build tools that are open and free technology for public use. Activities that aim to foster public
+            use, not private gain, are prioritised.
           </Typography>
         </li>
         <li>
           <Typography variant="body2" gutterBottom>
-            Work to ensure that the community is well-represented in the planning, design, and implementation of civic tech. This includes encouraging participation from women, minorities, and traditionally marginalized groups.
+            Work to ensure that the community is well-represented in the planning, design, and implementation of civic
+            tech. This includes encouraging participation from women, minorities, and traditionally marginalized groups.
           </Typography>
         </li>
         <li>
@@ -136,7 +145,8 @@ const CodeOfConduct: NextPage = () => (
         Unacceptable behaviour
       </Typography>
       <Typography variant="body2" gutterBottom>
-        To help community members understand the kind of behaviours that run counter to the culture we seek to foster, verbal, written or audiovisual comment or physical conduct based on the following is unwelcome: 
+        To help community members understand the kind of behaviours that run counter to the culture we seek to foster,
+        verbal, written or audiovisual comment or physical conduct based on the following is unwelcome:
       </Typography>
       <ul>
         <li>
@@ -156,7 +166,8 @@ const CodeOfConduct: NextPage = () => (
         </li>
         <li>
           <Typography variant="body2" gutterBottom>
-            sex (with or without sexual conduct and including pregnancy and sexual orientation involving transgender status/gender identity, and sex-stereotyping)
+            sex (with or without sexual conduct and including pregnancy and sexual orientation involving transgender
+            status/gender identity, and sex-stereotyping)
           </Typography>
         </li>
         <li>
@@ -234,16 +245,21 @@ const CodeOfConduct: NextPage = () => (
         Reporting unacceptable behaviour
       </Typography>
       <Typography variant="body2" gutterBottom>
-        The behaviours listed in this section undermine the culture of inclusion and respect that we are trying to build within the Hyperaudio community. We encourage you to report incidents of unacceptable behaviour to safespace@hyperaud.io so that we can take any necessary steps to ensure that we have a safe and welcoming team.
+        The behaviours listed in this section undermine the culture of inclusion and respect that we are trying to build
+        within the Hyperaudio community. We encourage you to report incidents of unacceptable behaviour to
+        safespace@hyperaud.io so that we can take any necessary steps to ensure that we have a safe and welcoming team.
       </Typography>
       <Typography variant="body2" gutterBottom>
-        The report will be sent to our community representative who will triage and call together appropriate members of the Hyperaudio team to decide upon the necessary action. You will be kept informed at all stages of the process and all details will be kept private and your anonymity guaranteed.
+        The report will be sent to our community representative who will triage and call together appropriate members of
+        the Hyperaudio team to decide upon the necessary action. You will be kept informed at all stages of the process
+        and all details will be kept private and your anonymity guaranteed.
       </Typography>
       <Typography variant="h5" component="h2" gutterBottom sx={{ mt: 6, mb: 3 }}>
         Credits
       </Typography>
       <Typography variant="body2" gutterBottom>
-        The Hyperaudio Team is greatly appreciative of the multiple sources that we drew from to build this Code of Conduct, including:
+        The Hyperaudio Team is greatly appreciative of the multiple sources that we drew from to build this Code of
+        Conduct, including:
       </Typography>
       <ul>
         <li>
@@ -263,7 +279,9 @@ const CodeOfConduct: NextPage = () => (
         </li>
         <li>
           <Typography variant="body2" gutterBottom>
-            <a href="https://www.mozilla.org/en-US/about/governance/policies/participation/">Mozilla Community Participation Guidelines</a>
+            <a href="https://www.mozilla.org/en-US/about/governance/policies/participation/">
+              Mozilla Community Participation Guidelines
+            </a>
           </Typography>
         </li>
       </ul>
