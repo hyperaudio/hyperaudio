fbfcd3deef42a147e2623c12ed9db40592b18f95
Laurian Gridinoc <laurian@gridinoc.name>
2023-03-10 00:47:20 +0200

    amplify updates

--- conferences/src/ui-components/index.js
+++ conferences/src/ui-components/index.js
@@ -7,9 +7,9 @@
 export { default as studioTheme } from "./studioTheme";
 export { default as MediaCreateForm } from "./MediaCreateForm";
 export { default as MediaUpdateForm } from "./MediaUpdateForm";
+export { default as UserCreateForm } from "./UserCreateForm";
+export { default as UserUpdateForm } from "./UserUpdateForm";
 export { default as ChannelCreateForm } from "./ChannelCreateForm";
 export { default as ChannelUpdateForm } from "./ChannelUpdateForm";
 export { default as RemixCreateForm } from "./RemixCreateForm";
 export { default as RemixUpdateForm } from "./RemixUpdateForm";
-export { default as UserCreateForm } from "./UserCreateForm";
-export { default as UserUpdateForm } from "./UserUpdateForm";
